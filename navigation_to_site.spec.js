416b4425fa64c61e5210f4a261e7a9fd4126cf02
Vasyl Samuliak <vasylsamuliak@gmail.com>
2025-09-30 13:13:39 +0300

    set the base url, capturing screenshots for failed tests

--- navigation_to_site.spec.js
+++ navigation_to_site.spec.js
@@ -1,9 +1,8 @@
 import { test as base } from '@playwright/test';
 
 export const test = base.extend({
-  page: async ({ browser }, use) => {
-    const page = await browser.newPage();
-    await page.goto('https://www.achadirect.com/'); // navigating to site
+  page: async ({ page, baseURL } , use) => {
+    await page.goto(baseURL); // navigating to site
     await use(page);
   }
 });
