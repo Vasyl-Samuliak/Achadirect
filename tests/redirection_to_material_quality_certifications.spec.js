a8aabb1261d1ae5ece6839c5cc17b088dfd843c5
Vasyl Samuliak <vasylsamuliak@gmail.com>
2025-10-03 13:19:00 +0300

    mobile and desktop: redirection to quality certifications page

--- tests/redirection_to_material_quality_certifications.spec.js
+++ tests/redirection_to_material_quality_certifications.spec.js
@@ -2,10 +2,14 @@
 import { expect } from '@playwright/test';
 import { test } from '../navigation_to_site.spec';
 
-test('Desktop: Redirection to quality certifications page after clicking on "Material Quality Certifications" link', 
+test('Redirection to quality certifications page after clicking on "Material Quality Certifications" link',
     async ({ page, isMobile }) => {
-    test.skip(isMobile);
-    await page.locator('#ui-id-7').click();
-    await page.getByRole('link', {name: "Material Quality Certifications", exact: true}).click();
-    await expect(page).toHaveURL('/material-quality-certifications');
-});
+        test.slow();
+        
+        if (isMobile)
+            await page.locator('span.nav-toggle').first().click();
+
+        await page.locator('a#ui-id-7').click();
+        await page.getByRole('link', { name: 'Material Quality Certifications', exact: true }).click();
+        await expect(page).toHaveURL('/material-quality-certifications');
+    });
