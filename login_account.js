5a1d59d6e27c762b320ed04ae121a31611b44281
Vasyl Samuliak <vasylsamuliak@gmail.com>
2025-10-04 21:31:41 +0300

    access my account

---
+++ login_account.js
@@ -0,0 +1,25 @@
+import { test as base } from '@playwright/test';
+
+export const test = base.extend({
+    page: async ({ page }, use) => {
+        test.slow();
+        await page.goto('/customer/account/login/');
+
+        await page.locator('#email').click();
+        await page.locator('#email').pressSequentially(process.env.TEST_LOGIN_EMAIL, { delay: 250 });
+        await page.waitForTimeout(300);
+
+        await page.locator('input[type="password"]').first().click();
+        await page.locator('input[type="password"]').first().pressSequentially(process.env.TEST_LOGIN_PASSWORD, { delay: 250 });
+        await page.waitForTimeout(300);
+
+        await page.getByRole('button', { name: 'DECLINE', exact: true }).click();
+        await page.waitForTimeout(300);
+        await page.getByRole('button', { name: 'Login', exact: true }).focus();
+        await page.waitForTimeout(300);
+
+        await page.getByRole('button', { name: 'Login', exact: true }).click();
+        await page.waitForURL('/');
+        await use(page);
+    }
+});
---
+++ tests/access_my_account.spec.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+import { expect } from '@playwright/test';
+import { test } from '../login_account';
+
+test('Access my account', async ({ page }) => {
+
+    test.slow();
+    await page.waitForLoadState('domcontentloaded');
+
+    await page.getByRole('link', { name: 'My Account', exact: true }).click();
+    await expect(page).toHaveURL('/customer/account/');
+});
