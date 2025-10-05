9ab6f30819a9fd6e336dc986639b735b2afef9a8
Vasyl Samuliak <vasylsamuliak@gmail.com>
2025-10-06 01:20:19 +0300

    adding and removing products

---
+++ tests/adding_removing_products.spec.js
@@ -0,0 +1,29 @@
+import { expect } from '@playwright/test';
+import { test } from '../login_account';
+
+test('Adding and removing products', async ({ page, isMobile }) => {
+
+    await page.waitForLoadState('domcontentloaded');
+
+    await page.locator('.product-item-link').first().click();
+    await page.waitForLoadState('domcontentloaded');
+
+    await page.locator('input#add-to-all').click();
+    await page.waitForTimeout(300);
+    await page.locator('input#add-to-all').first().pressSequentially('1', { delay: 250 });
+
+    if (isMobile) {
+        await page.getByRole('button', { name: 'Add all quantity', exact: true }).first().click();
+    }
+    else {
+        await page.getByRole('button', { name: 'Apply', exact: true }).first().click();
+    }
+
+    await page.waitForLoadState('domcontentloaded');
+    await page.locator('button.tocart').first().click();
+    await expect(page.locator('#confirmBox')).toBeVisible();
+
+    await page.locator('#product-remove-item-button').first().click();
+    await page.waitForLoadState('domcontentloaded');
+    await expect(page.locator('#confirmBox')).toBeVisible();
+});
