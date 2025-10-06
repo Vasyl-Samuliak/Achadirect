e99d07bd1978a6d6e5a23c8511f6b20130e0b0c6
Vasyl Samuliak <vasylsamuliak@gmail.com>
2025-10-06 23:55:52 +0300

    test only changing the currency

--- tests/changing_currency.spec.js
+++ tests/changing_currency.spec.js
@@ -23,7 +23,15 @@ const currencies = {
 };
 
 test('Changing the currency', async ({ page }) => {
-    test.setTimeout(180000);
+    test.setTimeout(300_000);
+
+    while (!(await page.getByRole('button', { name: 'DECLINE', exact: true }).isVisible())) {
+        await page.waitForTimeout(300);
+    }
+    await page.getByRole('button', { name: 'DECLINE', exact: true }).click();
+    await page.waitForLoadState('domcontentloaded');
+
+
     const default_currency = page.locator('#switcher-currency-trigger > strong > span:nth-of-type(2)');
     const default_currency_code = (await default_currency.innerText()).split('-')[0].trim();
 
@@ -33,6 +41,7 @@ test('Changing the currency', async ({ page }) => {
 
     const currency_switcher = page.locator('#switcher-currency');
     await currency_switcher.click();
+    await page.waitForLoadState('domcontentloaded');
 
     const currency_text_locator = 'div.switcher-options > ul > li > a > span.currency-text';
     const currencies_text = await currency_switcher.locator(currency_text_locator).allInnerTexts();
@@ -44,15 +53,18 @@ test('Changing the currency', async ({ page }) => {
         await currency_option.scrollIntoViewIfNeeded();
         await currency_option.click();
 
+
         await page.waitForURL('/');
-        await page.waitForTimeout(1000);
+        await page.waitForTimeout(3000);
+        await page.waitForLoadState('domcontentloaded');
 
         const changed_default_currency = page.locator('#switcher-currency-trigger > strong > span:nth-of-type(2)');
         expect(await changed_default_currency.innerText()).toContain(currency_code);
 
         // @ts-ignore
         expect(await page.locator('span.price').nth(2).innerText()).toContain(currencies[currency_code]);
-        await currency_switcher.click();
+        await page.locator('#switcher-currency').first().click();
+        await page.waitForLoadState('domcontentloaded');
     }
 
     const default_currency_option = page.locator(`li.currency-${default_currency_code}`).first();
@@ -60,7 +72,8 @@ test('Changing the currency', async ({ page }) => {
     await default_currency_option.click();
 
     await page.waitForURL('/');
-    await page.waitForTimeout(1000);
+    await page.waitForTimeout(3000);
+    await page.waitForLoadState('domcontentloaded');
 
     // @ts-ignore
     expect(await page.locator('span.price').nth(2).innerText()).toContain(currencies[default_currency_code]);
