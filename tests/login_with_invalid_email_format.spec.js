841a41b798e174cd76827b7ea3bbe8a0c78b0f7c
Vasyl Samuliak <vasylsamuliak@gmail.com>
2025-10-06 21:47:57 +0300

    test only login with invalid email format

--- tests/login_with_invalid_email_format.spec.js
+++ tests/login_with_invalid_email_format.spec.js
@@ -1,10 +1,18 @@
 // @ts-check
 require('dotenv').config();
-import {test, expect } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 
 
 test('Login with invalid email format', async ({ page }) => {
+    test.slow();
+
     await page.goto('/customer/account/login')
+
+    while (!(await page.getByRole('button', { name: 'DECLINE', exact: true }).isVisible())) {
+        await page.waitForTimeout(300);
+    }
+
+    await page.getByRole('button', { name: 'DECLINE', exact: true }).click();
 
     const email_field = page.locator('input#email').first();
     const password_field = page.locator('input#pass').first();
@@ -12,12 +20,12 @@ test('Login with invalid email format', async ({ page }) => {
     await email_field.click();
     // @ts-ignore
     await email_field.fill(process.env.TEST_INVALID_EMAIL_FORMAT);
-    
+
     await password_field.click();
     // @ts-ignore
     await password_field.fill(process.env.TEST_PASSWORD_FOR_INVALID_EMAIL_FORMAT);
 
-    const login_button = page.getByRole('button', {name: 'Login'});
+    const login_button = page.getByRole('button', { name: 'Login' });
     await login_button.click();
 
     await expect(page.locator('#email-error')).toBeVisible();
