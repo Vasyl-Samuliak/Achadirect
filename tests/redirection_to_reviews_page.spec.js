5c6beee7dea77144aa264fb56e66976408467c69
Vasyl Samuliak <vasylsamuliak@gmail.com>
2025-10-07 21:51:23 +0300

    test only redirection to reviews page

--- tests/redirection_to_reviews_page.spec.js
+++ tests/redirection_to_reviews_page.spec.js
@@ -3,6 +3,12 @@ import { expect } from '@playwright/test';
 import { test } from '../navigation_to_site.spec';
 
 test('Redirection to reviews page after clicking on "Customer Reviews" link', async ({ page }) => {
-    await page.locator('a.text-white').first().click();
+
+    while (!(await page.getByRole('button', { name: 'DECLINE', exact: true }).isVisible())) {
+        await page.waitForTimeout(300);
+    }
+    await page.getByRole('button', { name: 'DECLINE', exact: true }).click();
+
+    await page.getByRole('link', { name: 'CUSTOMER REVIEWS', exact: true }).click();
     await expect(page).toHaveURL('/reviews');
 });
